ba3be9c7d9a05f15cc8e64198b60ad2c9d328105
Koushik Mahanta <mahantakoushik80@gmail.com>
2024-06-19 14:57:45 +0530

    modified font sizes of the graphs

--- plot_height_vs_power.py
+++ plot_height_vs_power.py
@@ -27,6 +27,7 @@ data = pd.read_csv(csv_file_name)
 # plt.savefig('Fig_Height_vs_Total_Power.png')
 
 # Plot 2: Height vs NOMA_energy_efficiency and OMA_energy_efficiency
+font_size = 22
 plt.figure(figsize=(10, 6))
 plt.plot(data['height'], data['NOMA_energy_efficiency_40'], label='Non-exclusive clustering with NOMA_40 UEs', marker='v')
 plt.plot(data['height'], data['NOMA_energy_efficiency_60'], label='Non-exclusive clustering with NOMA_60 UEs', marker='o')
@@ -34,8 +35,10 @@ plt.plot(data['height'], data['ex-NOMA_energy_efficiency_40'], label='Exclusive 
 plt.plot(data['height'], data['ex-NOMA_energy_efficiency_60'], label='Exclusive clustering with NOMA_60 UEs', marker='X')
 plt.plot(data['height'], data['OMA_energy_efficiency_40'], label='No clustering with OMA_40 UEs', marker='+')
 plt.plot(data['height'], data['OMA_energy_efficiency_60'], label='No clustering with OMA_60 UEs', marker='P')
-plt.xlabel('UAV Height (m)')
-plt.ylabel('Energy Efficiency (in log scale)')
-plt.title('Height vs Energy Efficiency')
-plt.legend()
+plt.xlabel('UAV Height (m)', fontsize=font_size)
+plt.ylabel('Energy Efficiency (in log scale)', fontsize=font_size)
+plt.title('Height vs Energy Efficiency', fontsize=font_size)
+plt.legend(fontsize=font_size-9)
+plt.xticks(fontsize=font_size-8)
+plt.yticks(fontsize=font_size-8)
 plt.savefig('Fig_Height_vs_Energy_Efficiency.png')
--- plot_user_vs_power.py
+++ plot_user_vs_power.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+
 # Update rcParams to use Times New Roman
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['font.serif'] = 'Times New Roman'
@@ -13,16 +15,19 @@ plt.rcParams['mathtext.bf'] = 'Times New Roman:bold'
 csv_file_name = 'data_numberOfUEs_vs_power.csv'
 data = pd.read_csv(csv_file_name)
 
+font_size = 22
 # Plot 1: Number of UEs vs NOMA_total_power and OMA_total_power
 plt.figure(figsize=(10, 6))
 plt.plot(data['number_of_UEs'], [46]*len(data['number_of_UEs']), 'r--', label='maximum power')
 plt.plot(data['number_of_UEs'], data['NOMA_total_power'], label='Non-exclusive clustering with NOMA', marker='o')
 plt.plot(data['number_of_UEs'], data['ex-NOMA_total_power'], label='Exclusive clustering with NOMA', marker='o')
 plt.plot(data['number_of_UEs'], data['OMA_total_power'], label='No clustering with OMA', marker='s')
-plt.xlabel('Number of UEs')
-plt.ylabel('Total Transmit Power (dBm)')
-plt.title('Number of UEs vs Total Transmit Power')
-plt.legend()
+plt.xlabel('Number of UEs', fontsize=font_size)
+plt.ylabel('Total Transmit Power (dBm)', fontsize=font_size)
+plt.title('Number of UEs vs Total Transmit Power', fontsize=font_size)
+plt.legend(fontsize=font_size-8)
+plt.xticks(fontsize=font_size-8)
+plt.yticks(fontsize=font_size-8)
 plt.savefig('Fig_UEs_vs_TotalPower.png')
 
 # Plot 2: Number of UEs vs NOMA_energy_efficiency and OMA_energy_efficiency
@@ -30,9 +35,11 @@ plt.figure(figsize=(10, 6))
 plt.plot(data['number_of_UEs'], data['NOMA_energy_efficiency'], label='Non-exclusive clustering with NOMA', marker='o')
 plt.plot(data['number_of_UEs'], data['ex-NOMA_energy_efficiency'], label='Exclusive clustering with NOMA', marker='1')
 plt.plot(data['number_of_UEs'], data['OMA_energy_efficiency'], label='No clustering with OMA', marker='s')
-plt.xlabel('Number of UEs')
-plt.ylabel('Energy Efficiency (in log scale)')
-plt.title('Number of UEs vs Energy Efficiency')
-plt.legend()
+plt.xlabel('Number of UEs', fontsize=font_size)
+plt.ylabel('Energy Efficiency (in log scale)', fontsize=font_size)
+plt.title('Number of UEs vs Energy Efficiency', fontsize=font_size)
+plt.legend(fontsize=font_size-8)
+plt.xticks(fontsize=font_size-8)
+plt.yticks(fontsize=font_size-8)
 plt.savefig('Fig_UEs_vs_EnergyEfficiency.png')
 
